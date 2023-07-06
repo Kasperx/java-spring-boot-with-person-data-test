f86303271d30f33c4485945795331d6e5e404573
Kasperx <Titango2011@gmail.com>
2023-07-06 18:55:04 +0200

    works

--- src/main/java/com/example/PersonData/model/Employee.java
+++ src/main/java/com/example/PersonData/model/Employee.java
@@ -1,4 +1,4 @@
-package com.example.employeemanagementsystem.model;
+package com.example.PersonData.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
--- src/main/java/com/example/PersonData/repository/EmployeeRepository.java
+++ src/main/java/com/example/PersonData/repository/EmployeeRepository.java
@@ -1,9 +1,9 @@
-package com.example.employeemanagementsystem.repository;
+package com.example.PersonData.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.employeemanagementsystem.model.Employee;
+import com.example.PersonData.model.Employee;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
